31e088cf7e94dc177a5bff6c4200a698381d6c77
AravindSama <aravind.sama12@gmail.com>
2024-01-15 11:19:20 -0600

    Adding Cart Context

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import './index.scss';
 import App from './App';
 import { UserProvider } from "./contexts/user.context";
 import { ProductsProvider } from './contexts/products.context';
+import { CartProvider } from './contexts/cart.context';
 
 import reportWebVitals from './reportWebVitals';
 
@@ -15,11 +16,13 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
      <BrowserRouter>
-     <UserProvider>
-        <ProductsProvider>
-          <App />
-        </ProductsProvider>
-      </UserProvider>
+      <UserProvider>
+          <ProductsProvider>
+            <CartProvider>
+              <App />
+            </CartProvider>
+          </ProductsProvider>
+        </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
