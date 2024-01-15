abdfa64fcec9122c033ed524388208e8a9f238f6
AravindSama <aravind.sama12@gmail.com>
2024-01-15 08:18:48 -0600

    First Commit for Crwn Clothing

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
 import React from 'react';
+import { render } from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './index.scss';
 import App from './App';
+import { UserProvider } from "./contexts/user.context";
+import { ProductsProvider } from './contexts/products.context';
+
 import reportWebVitals from './reportWebVitals';
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+     <BrowserRouter>
+     <UserProvider>
+        <ProductsProvider>
+          <App />
+        </ProductsProvider>
+      </UserProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
